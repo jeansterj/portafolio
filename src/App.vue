ef79d7c03b2d8d48199d966cb680c7e25b024119
Ratzig <125646192+jeansterj@users.noreply.github.com>
2024-05-10 20:17:01 +0200

    d

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LandingPorta></LandingPorta>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LandingPorta from './components/LandingPorta.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LandingPorta
   }
 }
 </script>
@@ -21,6 +21,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body{
+  background-color: #25292B;
+
 }
 </style>
---
+++ src/components/LandingPorta.vue
@@ -0,0 +1,50 @@
+<template>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid justify-content-between">
+    <a class="navbar-brand" href="#"><img src="../assets/jeanPaulLogo.svg" alt="<Jean Paul>"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">CONOCEME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">HABILIDADES</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">PROYECTOS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">CONTACTO</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            IDIOMA
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">EN</a></li>
+            <li><a class="dropdown-item" href="#">ES</a></li>
+            <li><a class="dropdown-item" href="#">CAT</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
+
+</template>
+<script>
+
+
+</script>
