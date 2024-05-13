083cd88be3ac927bcbd27af045b323df1a4797e3
Ratzig <125646192+jeansterj@users.noreply.github.com>
2024-05-13 20:15:45 +0200

    g

--- src/components/LandingPorta.vue
+++ src/components/LandingPorta.vue
@@ -1,41 +1,64 @@
 <template>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid justify-content-between">
+<div class="bodyLanding">
+<nav class="navbar navbar-expand-lg bg-body-tertiary nabvarStyle">
+  <div class="container-fluid justify-content-around">
     <a class="navbar-brand" href="#"><img src="../assets/jeanPaulLogo.svg" alt="<Jean Paul>"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">HOME</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">CONOCEME</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">HABILIDADES</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">PROYECTOS</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">CONTACTO</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            IDIOMA
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">EN</a></li>
-            <li><a class="dropdown-item" href="#">ES</a></li>
-            <li><a class="dropdown-item" href="#">CAT</a></li>
-          </ul>
-        </li>
-      </ul>
+    <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+   
+      <a class="nav-link active mx-4" aria-current="page" href="#">HOME</a>
+
+      <a class="nav-link active mx-4" href="#">CONOCEME</a>
+
+      <a class="nav-link active mx-4" href="#">HABILIDADES</a>
+
+      <a class="nav-link active mx-4" href="#">PROYECTOS</a>
+
+      <a class="nav-link active mx-4" href="#">CONTACTO</a>
+      <div class="d-flex">
+      <a class="nav-link active" href="#">ES/</a>
+      <a class="nav-link active" href="#">EN/</a>
+      <a class="nav-link active" href="#">CAT</a>
+      </div>
     </div>
+    
   </div>
 </nav>
+
+<div class="container-fluid">
+  <div class="row justify-content-center align-items-center" style="height: 100vh;">
+    <div class="col-md-1 text-center">
+      <!-- Contenido a centrar -->
+      <div class="d-grid gap-3">
+        <!-- Imágenes a la izquierda -->
+        <img class="p-5" src="../assets/github.svg" alt="GitHub-logo">
+        <img class="p-5" src="../assets/twitter.svg" alt="twitter-logo">
+        <img class="p-5" src="../assets/linkedin-logo.svg" alt="linkedin-logo">
+      </div>
+    </div>
+    <div class="col-md-11 text-center">
+      <!-- Título y logo en el centro -->
+      <div class="d-grid">
+        <h1 class="text-white">DESARROLLADOR WEB</h1>
+        <img class="imgPortada" src="../assets/logoLanding.svg" alt="logoLanding">
+      </div>
+    </div>
+    <div class="arrow-scroll">
+  <div class="arrow"></div>
+  <div class="arrow"></div>
+  <div class="arrow"></div>
+</div>
+  </div>
+ 
+</div>
+
+
+
+
+</div>
+
 
 
 <AboutMe></AboutMe>
@@ -69,6 +92,10 @@ export default {
 </script>
 <style>
 
+.colorPrimary{
+  background-color: #35A2BD
+}
+
 .colorSecond{
   background-color: #D97B29
 }
@@ -76,5 +103,84 @@ export default {
 .colorAccion{
   background-color: #D4AF37
 }
+
+.nabvarStyle{
+  border-bottom-left-radius: 40px;
+    border-bottom-right-radius: 40px;
+}
+
+.bodyLanding{
+  display: flex;
+  margin: 0;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.imgPortada{
+  display: flex;
+  justify-self: center;
+  width: 40%;
+}
+
+.arrow-scroll{
+  position: relative;
+  height: 5em;
+}
+
+.arrow{
+  border: solid #D97B29;
+  border-width: 0 10px 10px 0;
+  display: inline-block;
+  padding: 30px;
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: 
+  translateX(-50%)
+  rotate(45deg); 
+}
+
+.arrow:nth-child(2){
+  animation: 
+  arrow1 1.5s
+  ease-in-out infinite;
+}
+
+.arrow:nth-child(3){
+  animation: 
+  arrow1 1.5s
+  ease-in-out infinite;
+}
+
+@keyframes scroll {
+  0% {
+    bottom: 80%;
+    opacity: 1;
+  }
+  100% {
+    bottom: 20%;
+    opacity: 0;
+    }
+}
+
+@keyframes arrow1 {
+ 
+  100% {
+    opacity: 0;
+    top: 100%;
+    }
+}
+
+@keyframes arrow2 {
+ 
+ 100% {
+   opacity: 0;
+   top: 50%;
+
+   }
+}
+
 
 </style>
--- src/components/SkillsPorta.vue
+++ src/components/SkillsPorta.vue
@@ -1,30 +1,31 @@
 <template>
-<div class="d-none">
+<div class="container-fluid my-5">
 
-    <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-        <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
-        <label class="btn btn-outline-primary" for="btnradio1">PERSONALES</label>
+    <div class="btn-group text-white" role="group" aria-label="Basic radio toggle button group">
+        <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" :checked="personal" @click="prueba('personal')">
+        <label class="btn btn-outline" for="btnradio1">PERSONALES</label>
       
-        <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-        <label class="btn btn-outline-primary" for="btnradio2">HABILIDADES</label>
+        <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off" :checked="habilidades" @click="prueba('habilidades')">
+        <label class="btn btn-outline" for="btnradio2">HABILIDADES</label>
       
-        <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-        <label class="btn btn-outline-primary" for="btnradio3">HERRAMIENTAS</label>
+        <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off" :checked="herramientas" @click="prueba('herramientas')">
+        <label class="btn btn-outline" for="btnradio3">HERRAMIENTAS</label>
       </div>
 
-    <div >
-        <p>Caracteristicas personales que me crean como profesional.</p>
-
-        <button v-for="skill in skillPersonal" :key="skill.id"> {{ skill.skill }}</button>
+    <div v-if="personal"  class="container text-white">
+        <p class="my-5">Caracteristicas personales que me crean como profesional.</p>
+        <div class="button-container my-4">
+        <button class="btn text-white colorPrimary" v-for="skill in skillPersonal" :key="skill.id"> {{ skill.skill }}</button>
+    </div>
     </div>
 
-    <div>
-        <p>Habilidades con las que he desarrollado mis diferentes proyectos.</p>
+    <div v-else-if="habilidades" class="container text-white">
+        <p class="my-5">Habilidades con las que he desarrollado mis diferentes proyectos.</p>
 
     </div>
 
-    <div>
-        <p>Herramientas que han permitdo mejoras en el desarrollo.</p>
+    <div v-else-if="herramientas" class="container text-white">
+        <p class="my-5">Herramientas que han permitdo mejoras en el desarrollo.</p>
     </div>
     
 </div>
@@ -42,8 +43,90 @@ export default {
                 {id: 4, skill: 'EMPATIA'},
                 {id: 5, skill: 'INNOVACIÓN'},
                 {id: 6, skill: 'ADAPTABILIDAD'}
+            ],
+            personal: true,
+            habilidades: false,
+            herramientas: false,
+            skillHabilidades: [
+            {id: 1, name: 'vue-logo', src:''},
+            {id: 2, name: 'html-logo', src:''},
+            {id: 3, name: 'css-logo', src:''},
+            {id: 4, name: 'js-logo', src:''},
+            {id: 5, name: 'java-logo', src:''},
+            {id: 6, name: 'laravel-logo', src:''},
+            {id: 6, name: 'bootstrap-logo', src:''},
+
+            ],
+            skillHerramientas: [
+            {id: 1, name: 'figma-logo', src:''},
+            {id: 2, name: 'clickUp-logo', src:''},
+            {id: 3, name: 'jira-logo', src:''},
+            {id: 4, name: 'visual-Estudio-Code-logo', src:''},
+            
             ]
         }
     },
+    methods:{
+        prueba(opcion){
+        if (opcion === 'personal') {
+            this.personal = true;
+            this.habilidades = false;
+            this.herramientas = false;
+        } else if (opcion === 'habilidades') {
+            this.personal = false;
+            this.habilidades = true;
+            this.herramientas = false;
+        } else if (opcion === 'herramientas') {
+            this.personal = false;
+            this.habilidades = false;
+            this.herramientas = true;
+        }
+    }
+    }
 }
 </script>
+<style>
+  .btn-outline {
+    color: white;
+    border-color: white;
+  }
+
+  .btn-outline:hover {
+  color: white !important; 
+  background-color: #DB995C !important; 
+  border-color: white !important; 
+}
+
+.btn.text-white.colorPrimary:hover{
+    background-color: #35A2BD !important; 
+    color: white !important; 
+    border-color: white !important; 
+
+}
+  
+.btn-outline-primary {
+  background-color: #D97B29;
+  color: white; 
+  border-color: white; 
+}
+
+.btn-check:checked + .btn, .btn.active, .btn.show, .btn:first-child:active, :not(.btn-check) + .btn:active {
+  background-color: #D97B29;
+  border-color: white; 
+}
+
+.btn-check:checked + .btn {
+  color: white;
+  border-color: white;
+}
+
+
+.button-container {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(3, auto);
+    gap: 20px; 
+  }
+
+
+</style>
